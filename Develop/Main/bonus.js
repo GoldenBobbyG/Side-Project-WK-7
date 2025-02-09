1c818e400e3e9e0d443ab6ba6bbe83aea7176d47
Robb Strick <stricklandmba2011@gmail.com>
2025-02-09 16:00:55 -0500

    added files

---
+++ Develop/Main/bonus.js
@@ -0,0 +1,41 @@
+import inquirer from 'inquirer';
+// Node v10+ includes a promises module as an alternative to using callbacks with file system methods.
+// Use writeFileSync method to use promises instead of a callback function
+const fs = require('fs');
+const { writeFile } = fs.promises;
+
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'input',
+      name: 'location',
+      message: 'Where are you from?',
+    },
+    {
+      type: 'input',
+      name: 'hobby',
+      message: 'What is your favorite hobby?',
+    },
+    {
+      type: 'input',
+      name: 'food',
+      message: 'What is your favorite food?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
+    },
+    {
+      type: 'input',
+      name: 'linkedin',
+      message: 'Enter your LinkedIn URL',
+
+    },
+  ]);
+};
